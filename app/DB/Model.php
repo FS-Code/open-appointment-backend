b561a3afdaacef57c88f760aba8cbf4fb5c163a4
asker <askeraliyev24+git@pm.me>
2023-05-26 12:43:31 +0400

    delete unnecessary prepare method

--- app/DB/Model.php
+++ app/DB/Model.php
@@ -2,13 +2,6 @@
 
 namespace App\DB;
 
-use App\Core\DB;
-use PDOStatement;
-
 class Model
 {
-    public static function prepare( $query, $args ): PDOStatement
-    {
-        return DB::prepare( $query, $args );
-    }
 }
