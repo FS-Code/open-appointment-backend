7f9b46125f592f7ecd476d19f4a086ec096f0069
Sebuhi <116587943+Karz4@users.noreply.github.com>
2023-05-27 00:53:24 -0700

    Update Helper.php
    Duzelis edildi

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,32 +1,30 @@
-<?php 
-  
- namespace App\Helpers; 
-  
- class DateFormatHelper { 
-    
-     public static function secondsToHumanReadable(int $duration): string 
-     { 
-         $hours = floor($duration / 3600); 
-         $minutes = floor(($duration % 3600) / 60); 
-         $seconds = $duration % 60; 
-          
-         $output = ''; 
-          
-         if ($hours > 0) { 
-             $output .= $hours . 'hr'; 
-         } 
-          
-         if ($minutes > 0) { 
-             if ($output !== '') { 
-                 $output .= ' '; 
-             } 
-             $output .= $minutes . 'min'; 
-         } 
-          
-         if ($output === '') { 
-             $output = $seconds . 'sec'; 
-         } 
-          
-         return $output; 
-     } 
- }
+<?php
+
+function secondsToHumanReadable(int $duration): string
+{
+    $hours = floor($duration / 3600);
+    $minutes = floor(($duration % 3600) / 60);
+    $seconds = $duration % 60;
+
+    $output = '';
+
+    if ($hours > 0) {
+        $output .= $hours . 'hr';
+    }
+
+    if ($minutes > 0) {
+        if ($output !== '') {
+            $output .= ' ';
+        }
+        $output .= $minutes . 'min';
+    }
+
+    if ($output === '') {
+        $output = $seconds . 'sec';
+    }
+
+    return $output;
+}
+
+echo secondsToHumanReadable(3665); // * Numune cavab 1 saat 1 deq 5 saniye cixacaq
+?>
