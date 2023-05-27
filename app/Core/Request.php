675e2e4649200773c1276546e4f3c245bb62c6bd
Sahrux <ssahrux@gmail.com>
2023-05-28 01:00:05 +0400

    extra req methods

--- app/Core/Request.php
+++ app/Core/Request.php
@@ -60,4 +60,70 @@ class Request
     {
         return '';
     }
+
+    private static function sanitize($input){
+
+        $input = trim($input);
+        
+        $input = preg_replace('/\s+/', ' ', $input);
+        
+        $input = strip_tags($input);
+        
+        $input = preg_replace('/[^a-zA-Z0-9\s]/', '', $input);
+        
+        $input = strtolower($input);
+        
+        return $input;
+    }
+
+    public static function get(string $key, $default = null): ?string {
+
+        if (isset($_GET[$key])) {
+
+            return self::sanitize($_GET[$key]);
+
+        }
+        
+        return $default;
+    }
+
+    public static function post(string $key, $default = null): ?string {
+
+        if (isset($_POST[$key])) {
+
+            return self::sanitize($_POST[$key]);
+
+        }
+        
+        return $default;
+    }
+
+    public static function cookie(string $key, $default = null): ?string {
+
+        if (isset($_COOKIE[$key])) {
+
+            return self::sanitize($_COOKIE[$key]);
+
+        }
+        
+        return $default;
+    }
+
+    function has(string $type ,string|array $key): bool {
+
+        $input_variables = $type === "POST" || $type === "post" ? $_POST : $_GET;
+
+        if (is_array($key)) {
+            foreach ($key as $i) {
+                if (!isset($input_variables[$i])) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return isset($input_variables[$key]);
+        }
+    }
+    
+    
 }
