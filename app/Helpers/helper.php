38c1c918f8199993326775702846e5cb90221d80
Mehrelizade <129266364+Mehrelizade@users.noreply.github.com>
2023-05-28 06:13:48 -0700

    Add files via upload

---
+++ app/Helpers/helper.php
@@ -0,0 +1,33 @@
+<?php
+namespace MyProject\Helpers;
+
+class Helper {
+    public static function formatDuration(int $duration): string {
+        $days = floor($duration / (24 * 60));
+        $hours = floor(($duration % (24 * 60)) / 60);
+        $minutes = $duration % 60;
+
+        $formattedDuration = '';
+
+        if ($days > 0) {
+            $formattedDuration .= $days . ' days ';
+        }
+
+        if ($hours > 0) {
+            $formattedDuration .= $hours . ' hours ';
+        }
+
+        if ($minutes > 0) {
+            $formattedDuration .= $minutes . ' minutes';
+        }
+
+
+        return trim($formattedDuration);
+    }
+}
+
+// Static method call
+$duration = 345678;
+$formatted = Helper::formatDuration($duration);
+echo $formatted;
+?>
