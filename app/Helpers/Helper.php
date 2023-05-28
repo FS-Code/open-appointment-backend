a7c492d6402588270623f03dc69d0ee839d30063
Tural Gasimov <turalgasimov@proton.me>
2023-05-29 02:00:46 +0400

    my task submission

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,7 +2,63 @@
 
 namespace App\Helpers;
 
-class Helper
-{
+class Helper {
+
+    static function secondsToTime(int $duration) : string {
+
+        //Daxil edilən ədədin gün, saat və dəqiqə formatına çevrilməsi;
+
+        $toDays = (int)($duration / (3600 * 24));
+        $toHours = (int)(($duration % (3600 * 24)) / 3600);
+        $toMinutes = (int)(($duration % 3600) / 60);
+        $time = "";
+
+        //Günlərin sayının əlavə edilməsi (qiymət 0-dan böyükdürsə);
+
+        if ($toDays > 0) {
+
+            $time .= $toDays . "d";
+
+        }
+
+        //Saatların sayının əlavə edilməsi (qiymət 0-dan böyükdürsə);
+
+        if ($toHours > 0) {
+
+            if ($time != "") {
+
+                $time .= " ";
+
+            }
+
+            $time .= $toHours . "hr";
+
+        }
+
+        //Dəqiqələrin sayının əlavə edilməsi (qiymət 0-dan böyükdürsə);
+
+        if ($toMinutes > 0) {
+
+            if ($time != "") {
+
+                $time .= " ";
+
+            }
+
+            $time .= $toMinutes . "min";
+
+        }
+
+        //Daxiletmə olmadıqda mesaj verilməsi;
+
+        if ($time == "") {
+
+            echo "No time is left..";
+
+        }
+
+        return $time . "\n";
+
+    }
 
 }
