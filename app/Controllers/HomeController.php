6056d6a94f8dec99c52ce2989da278b1ac2df20d
Ferid <ferid1.nesibzade@mail.ru>
2023-05-28 16:37:39 +0400

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,14 +8,18 @@ class HomeController
 {
     public static function index(): string
     {
-        return[
-            DateTime::timeToReadable(60*60*24*3); // 3 days
-            DateTime::timeToReadable(60*60*24*1); // 1 day
-            DateTime::timeToReadable(63); // 1 min 3 secs
-            DateTime::timeToReadable(60*4); // 4 mins
-            DateTime::timeToReadable(60*60*5+60); // 5 hrs 1 min
-            
-        ]
+        echo '<pre>';
+        print_r([
+            DateTime::timeToReadable(60*60*24*3), // 3 days
+            DateTime::timeToReadable(60*60*24*1), // 1 day
+            DateTime::timeToReadable(63), // 1 min 3 secs
+            DateTime::timeToReadable(60*4), // 4 mins
+            DateTime::timeToReadable(60*60*5+60), // 5 hrs 1 min
+            DateTime::timeToReadable(60*62*24+2), // 1 day  48 mins 2 secs
+            DateTime::timeToReadable(1685228727), // 26 days 23 hrs 5 mins 27 secs
+        ]);
+        echo '<pre>';
+        exit();
             
 //        return 'I\'m an index function!';
     }
