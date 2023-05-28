3600352041000c3e54856876eae9cedebf337a48
Aytac Ali <aytacaliyeva133@gmail.com>
2023-05-29 01:25:42 +0400

    return something if the time lower than a minute

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -9,31 +9,36 @@ class Helper
 	/**
 	 * @throws Exception
 	 */
-	public static function secondsToHumanReadable( int $duration ): string
+	public static function secondsToHumanReadable ( int $duration ) : string
 	{
-		if ( $duration < 0 ) {
+		if ( $duration < 0 )
+		{
 			throw new Exception( 'Time cannot be lower than zero' );
 		}
 
-		$days    = floor( $duration / 86400 );
-		$hours   = floor( ( $duration - $days * 86400 ) / 3600 );
-		$minutes = floor( ($duration / 60 ) % 60 );
+		if ( $duration < 60 ) {
+			$result = 'The time is lower than a minute';
+		} else {
+			$days    = floor( $duration / 86400 );
+			$hours   = floor( ( $duration - $days * 86400 ) / 3600 );
+			$minutes = floor( ( $duration / 60 ) % 60 );
 
-		$days = ( $days == 0 ? '' : ( $days > 1 ? $days . 'days ' : $days . 'd ' ) );
-		$hours = ( $hours == 0 ? '' : ( $hours > 1 ? $hours . 'hrs ' : $hours . 'hr ' ));
-		$minutes = ( $minutes == 0 ? '' : ( $minutes > 1 ? $minutes . 'mins' : $minutes . 'min' ) );
+			$days    = ( $days == 0 ? '' : $days . ( $days > 1 ? 'days ' : 'd ' ) );
+			$hours   = ( $hours == 0 ? '' : $hours . ( $hours > 1 ? 'hrs ' : 'hr ' ) );
+			$minutes = ( $minutes == 0 ? '' : $minutes . ( $minutes > 1 ? 'mins' : 'min' ) );
 
-		if ( $days != 0 )
-		{
-			$result = $days . $hours . $minutes;
-		}
-		else if ( $hours != 0 )
-		{
-			$result = $hours . $minutes;
-		}
-		else
-		{
-			$result = $minutes;
+			if ( $days != 0 )
+			{
+				$result = $days . $hours . $minutes;
+			}
+			else if ( $hours != 0 )
+			{
+				$result = $hours . $minutes;
+			}
+			else
+			{
+				$result = $minutes;
+			}
 		}
 
 
