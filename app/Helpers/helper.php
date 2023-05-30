7138d84222b63a5ebad9cc30582e4cdc95eebc07
Cavidan03 <aa4060918@gmail.com>
2023-05-30 16:49:03 +0400

    task

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -14,21 +14,19 @@ class Helper {
     
         $result = '';
         if ($days > 0) {
-            $result .= $days . 'd ';
+            $result .= $days . ($days % 2 == 1 ? 'd ' : 'days ');
         }
         if ($hours > 0) {
-            $result .= $hours . 'hr ';
+            $result .= $hours . ($hours %2== 1 ? 'hr ' : 'hrs ');
         }
         if ($minutes > 0) {
-            $result .= $minutes . 'mins';
+            $result .= $minutes . ($minutes%2==1 ? 'min' : 'mins');
         }
     
         return trim($result);
     }
 }
- echo Helper::secondsToHumanReadable(3600);
- echo Helper::secondsToHumanReadable(1800);
- echo Helper::secondsToHumanReadable(5400);
- echo Helper::secondsToHumanReadable(7200);
+
+
 
 ?>
