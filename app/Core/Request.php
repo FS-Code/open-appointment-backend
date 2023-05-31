0af53882adfa1b7b2e6e3fd390b394c88eeab0d2
My-projects-dev <anar.ismayilov.1991@inbox.ru>
2023-05-31 15:16:01 +0400

    again

--- app/Core/Request.php
+++ app/Core/Request.php
@@ -10,32 +10,29 @@ class Request
 
     public static function path(): string
     {
-        $path     = $_SERVER[ 'REQUEST_URI' ] ?? '/';
-        $position = strpos( $path, '?' );
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
 
-        if ( $position === false )
-        {
-            return trim( $path, '/' );
+        if ($position === false) {
+            return trim($path, '/');
         }
 
-        return trim( substr( $path, 0, $position ), '/' );
+        return trim(substr($path, 0, $position), '/');
     }
 
     public static function method(): string
     {
-        return strtolower( $_SERVER[ 'REQUEST_METHOD' ] );
+        return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
     public static function query(): array
     {
-        if ( !! self::$query )
-        {
+        if (!!self::$query) {
             return self::$query;
         }
 
-        foreach ( $_GET as $k => $v )
-        {
-            self::$query[ $k ] = filter_input( INPUT_GET, $k, FILTER_SANITIZE_SPECIAL_CHARS );
+        foreach ($_GET as $k => $v) {
+            self::$query[$k] = filter_input(INPUT_GET, $k, FILTER_SANITIZE_SPECIAL_CHARS);
         }
 
         return self::$query;
@@ -43,14 +40,12 @@ class Request
 
     public static function body(): array
     {
-        if ( !! self::$body )
-        {
+        if (!!self::$body) {
             return self::$body;
         }
 
-        foreach ( $_POST as $k => $v )
-        {
-            self::$body[ $k ] = filter_input( INPUT_POST, $k, FILTER_SANITIZE_SPECIAL_CHARS );
+        foreach ($_POST as $k => $v) {
+            self::$body[$k] = filter_input(INPUT_POST, $k, FILTER_SANITIZE_SPECIAL_CHARS);
         }
 
         return self::$body;
@@ -59,5 +54,42 @@ class Request
     public static function rawBody(): string
     {
         return '';
+    }
+
+    public static function get(string $key, $default = null): string|null
+    {
+        return filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS) ?? $default;
+    }
+
+    public static function post(string $key, $default = null): string|null
+    {
+        return filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS) ?? $default;
+    }
+
+    public static function cookie(string $key, $default = null): string|null
+    {
+        return filter_input(INPUT_COOKIE, $key, FILTER_SANITIZE_SPECIAL_CHARS) ?? $default;
+    }
+
+    public static function has(string $type, $key): bool
+    {
+        switch ($type) {
+            case 'GET':
+                return is_array($key) ? self::hasKeysInArray($key, $_GET) : isset($_GET[$key]);
+            case 'POST':
+                return is_array($key) ? self::hasKeysInArray($key, $_POST) : isset($_POST[$key]);
+            default:
+                return false;
+        }
+    }
+
+    private static function hasKeysInArray(array $keys, array $method): bool
+    {
+        foreach ($keys as $key) {
+            if (!isset($method[$key])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
