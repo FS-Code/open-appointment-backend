235aed14bb47ee1f97102a6d9684efbf23be9062
sadafimanova <112188893+sadafimanova@users.noreply.github.com>
2023-05-31 22:14:35 +0400

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,26 +2,38 @@
 
 namespace App\Helpers;
 
-class Helper{
-    public static  function secondsToHumanReadable(int $duration): string {
-        $secondsOfMinute=60;
-        $secondsOfHour=3600;
-        $secondsOfDay=86400;
+class Helper {
+    public static function secondsToHumanReadable(int $duration): string {
+        $secondsOfMinute = 60;
+        $secondsOfHour = 3600;
+        $secondsOfDay = 86400;
+    
+        if ($duration <= 0) {
+            throw new \InvalidArgumentException("Duration must be a positive integer.");
+        }
         
-        $days=floor($duration/$secondsOfDay);
-        $duration%=$secondsOfDay;
+        $days = floor($duration / $secondsOfDay);
+        $duration %= $secondsOfDay;
         
-        $hours=floor($duration/$secondsOfHour);
-        $duration%=$secondsOfHour;
+        $hours = floor($duration / $secondsOfHour);
+        $duration %= $secondsOfHour;
         
-        $minutes=floor($duration/$secondsOfMinute);
+        $minutes = floor($duration / $secondsOfMinute);
         
-        $result="";
+        $result = "";
         
-        $result .= $days > 1 ? "days": "d";
-        $result .= $hours > 1 ? "hrs": "hr";
-        $result .= $minutes > 1 ? "mins": "min";
-
-        return $result;
+        if ($days > 0) {
+            $result .= $days . ($days > 1 ? " days " : " day ");
+        }
+        
+        if ($hours > 0) {
+            $result .= $hours . ($hours > 1 ? " hrs " : " hr ");
+        }
+        
+        if ($minutes > 0) {
+            $result .= $minutes . ($minutes > 1 ? " mins" : " min");
+        }
+        
+        return trim($result);
     }
 }
