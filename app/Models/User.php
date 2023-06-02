6fe5f02e28dc03f9431e4c59f32b3943ccee4b07
CamalPashazade <camal24011992@gmail.com>
2023-06-02 14:51:06 +0400

    task updateUserEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,12 @@
 
 namespace App\Models;
 
+use PDO;
+use Exception;
+use App\Core\DB;
 use App\DB\Model;
+use PDOException;
+
 
 class User extends Model
 {
@@ -14,5 +19,38 @@ class User extends Model
     public static function remove( int $id ):bool
     {
         return true;
+    }
+
+    public function updateUserEmail( int $id, string $email): void
+    {
+        try {
+            $connection = DB::DB();
+            $checkUser = $connection->prepare("SELECT id FROM users WHERE id = :id");
+            $checkUser->bindParam(":id",$id);
+            $checkUser->execute();
+            $checkUser = $checkUser->fetch();
+            
+            if($checkUser){
+                $checkUserEmail = $connection->prepare("SELECT email FROM users WHERE email=:email");
+                $checkUserEmail->bindParam(":email",$email);
+                $checkUserEmail->execute();
+                $checkUserEmail = $checkUserEmail->fetch();
+    
+                if (!$checkUserEmail){
+                    $userEmailUpdate = $connection->prepare("UPDATE users SET email = :email WHERE id = :id");
+                    $userEmailUpdate->bindParam(":email",$email);
+                    $userEmailUpdate->bindParam(":id",$id);
+                    $userEmailUpdate->execute();
+    
+                } else {
+                    throw new Exception("This email already exists");
+                }
+                
+            } else {
+                throw new Exception("User not found");
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
