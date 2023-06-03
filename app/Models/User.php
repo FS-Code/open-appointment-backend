be1c62a8af224b8be9086be436cdc8626d74e2e9
Tahir Guliyev <tahirguliyev95@gmail.com>
2023-06-03 13:17:16 +0400

    feat: add login verification in User model #50
    Added 'getUserByLoginPass' method to the User model. This method checks if there is a user in the database that matches a given email and hashed password. If a match is found, it returns the user's information (except the password) as an object. If no match is found, it throws an exception.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use App\DB\Model;
+use App\Core\DB;
+use PDO;
+use Exception;
 
 class User extends Model
 {
@@ -14,5 +17,23 @@ class User extends Model
     public static function remove( int $id ):bool
     {
         return true;
+    }
+
+    public static function getUserByLoginPass(string $email, string $password): object
+    {
+        $query = "SELECT * FROM user WHERE email = :email";
+        $stmt = DB::DB()->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+        if (!$user || $user->password !== $password) {
+            throw new Exception('User not found by given credentials');
+        }   
+
+        unset($user->password);
+
+        return $user;
     }
 }
