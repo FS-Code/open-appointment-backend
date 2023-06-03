6880e465a2ea21706d787a880a6fbf17a9ea4057
Israfil Rahimbayli <israfil.rahimbeyli@gmail.com>
2023-06-03 15:45:56 +0400

    fist commit

---
+++ app/Integrations/Telegram.php
@@ -0,0 +1,44 @@
+<?php
+
+class Telegram
+{
+    public static function sendMessage(string $botToken, int $chatId, string $message): void
+    {
+        $telegramApiUrl = "https://api.telegram.org/bot{$botToken}/sendMessage";
+        $postFields = [
+            'chat_id' => $chatId,
+            'text'    => $message
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $telegramApiUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response     = curl_exec($ch);
+        $errorCode    = curl_errno($ch);
+        $errorMessage = curl_error($ch);
+        curl_close($ch);
+
+        if ($errorCode) {
+            throw new Exception("Curl error: {$errorMessage}", $errorCode);
+        }
+
+        $jsonResponse = json_decode($response, true);
+
+        if (!$jsonResponse || !$jsonResponse['ok']) {
+            throw new Exception('Telegram API error: ' . ($jsonResponse['description'] ?? 'unknown error'));
+        }
+    }
+}
+
+// Usage example:
+try {
+    Telegram::sendMessage('<bot-token>', 123456789, 'Hello!');
+} catch (Exception $e) {
+    // Handle exceptions
+    echo "Error: " . $e->getMessage();
+}
