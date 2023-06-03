f41cc649b7f34cd21a0c19ca18d1f2370d549395
jalilverdiyev <calilverdiyev2@gmail.com>
2023-06-03 19:54:32 +0400

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,38 @@
 namespace App\Models;
 
 use App\DB\Model;
+use Exception;
+use PDO;
+use App\Core\DB;
 
 class User extends Model
 {
-    public static function add( array $params ):int
+    public static function add(array $params): int
     {
         return 0;
     }
 
-    public static function remove( int $id ):bool
+    public static function remove(int $id): bool
     {
         return true;
     }
+
+    public static function getUserById(int $id): object
+    {
+        $db       = DB::DB();
+        $query    = "SELECT id, email FROM user WHERE id= :id";
+        $prepared = $db->prepare($query);
+        $prepared->bindParam(":id", $id, PDO::PARAM_INT);
+        $prepared->execute();
+
+        $result = $prepared->fetchObject();
+
+        if ($result) {
+            return $result;
+        }
+
+        throw new Exception("User not found by given id");
+    }
+
+
 }
