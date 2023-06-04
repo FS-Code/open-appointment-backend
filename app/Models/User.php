034e8c87eeb4ef6b80958bfc0c353dbc799b18d7
Mehrelizade <129266364+Mehrelizade@users.noreply.github.com>
2023-06-04 02:24:47 -0700

    Add files via upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,39 @@
 namespace App\Models;
 
 use App\DB\Model;
+use Exception;
+use PDO;
+use App\Core\DB;
 
 class User extends Model
 {
-    public static function add( array $params ):int
+    public static function add(array $params): int
     {
+        
         return 0;
     }
 
-    public static function remove( int $id ):bool
+    public static function remove(int $id): bool
     {
+        
         return true;
+    }
+
+    public static function getUserById(int $id): object
+    {
+        $db = DB::DB();
+        $query = "SELECT * FROM users WHERE id = :id";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+        if ($user) {
+            unset($user->password);
+            return $user;
+        }
+
+        throw new Exception("User not found by given id");
     }
 }
