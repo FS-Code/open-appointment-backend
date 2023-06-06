c62ec30d9a6bf2247168b5a279802bd18624781f
My-projects-dev <anar.ismayilov.1991@inbox.ru>
2023-06-06 20:10:56 +0400

    Revert "Revert "env""
    This reverts commit 9f00af7b46fe124e918d2748622eb727b88c33d4.

--- app/Integrations/Mailer.php
+++ app/Integrations/Mailer.php
@@ -1,29 +1,30 @@
 <?php
 
 namespace App\Integrations;
-require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+use App\Core\Env;
 
 class Mailer
 {
     public static function sendMessage(string $to, string $subject, string $message): void
     {
+        Env::init();
         $mail = new PHPMailer(true);
 
         try {
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;
             $mail->isSMTP();
-            $mail->Host       = 'smtp.example.com';
+            $mail->Host       = $_ENV['SMTP_HOST'];
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'my_email@example.com';
-            $mail->Password   = 'my_password';
+            $mail->Username   = $_ENV['SMTP_USERNAME'];
+            $mail->Password   = $_ENV['SMTP_PASSWORD'];
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port       = 587;
+            $mail->Port       = $_ENV['SMTP_PORT'];
 
-            $mail->setFrom('my_email@example.com', 'My Name');
+            $mail->setFrom($_ENV['FROM_EMAIL'], $_ENV['FROM_NAME']);
             $mail->addAddress($to);
 
             $mail->isHTML(true);
