766b3c7a14fc0530d79147e669269414ee0a38cc
Israfil Rahimbayli <israfil.rahimbeyli@gmail.com>
2023-06-11 13:08:52 +0400

    duzelis olundu

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -2,32 +2,49 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
-use App\Core\Request;
-use App\Core\Response;
 
-class LoginController
+use App\Core\Response;
+use App\Core\Request;
+use App\Models\User;
+use App\Helpers\AuthHelper;
+
+class UserController
 {
     public function login()
     {
-        $email = Request::post('email');
+        $email    = Request::post('email');
         $password = Request::post('password');
 
-        $user = User::getUserByEmail($email);
+        try {
 
-        if (!$user || !password_verify($password, $user['password'])) {
-            Response::setStatusBadRequest();
-            return ['error' => 'Email or password is not correct'];
-        }
+            $user = User::getUserByEmail($email);
 
-        Response::setStatusOk();
-        return [
-            'data' => [
-                'user' => [
+            if ($user && password_verify($password, $user['password'])) {
+
+                $token = AuthHelper::generateJWT([
                     'id' => $user['id'],
-                    'email' => $user['email']
-                ]
-            ]
-        ];
+                    'email' => $user['email'],
+                ]);
+
+                $responseData = [
+                    'user' => [
+                        'id' => $user['id'],
+                        'email' => $user['email'],
+                    ],
+                    'token' => $token,
+                ];
+
+                Response::setStatusOk();
+                Response::json($responseData);
+            } else {
+
+                Response::setStatusBadRequest();
+                Response::json(['error' => 'Email or password is incorrect']);
+            }
+        } catch (Exception $e) {
+
+            Response::setStatusBadRequest();
+            Response::json(['error' => $e->getMessage()]);
+        }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use App\DB\Model;
-use App\Core\DB;
-use PDO;
+
 
 
 
@@ -20,52 +19,5 @@ class User extends Model
         return true;
     }
 
-    private $id;
-    private $email;
-    private $password;
-
-    public function __construct($id, $email, $password)
-    {
-        $this->id = $id;
-        $this->email = $email;
-        $this->password = $password;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-
-
-    public function getUserByEmail($email)
-    {
-        $db = DB::DB();
-        $stmt = $db->prepare('SELECT * FROM users WHERE email = :email');
-        $stmt->bindParam(':email', $email);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public function getUserById($id)
-    {
-        $db = DB::DB();
-        $stmt = $db->prepare('SELECT * FROM users WHERE id = :id');
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
 
 }
