cf8a5e60a2027f2e41e692eefa33b070ae3f8186
devalirzayev <62056172+devalirzayev@users.noreply.github.com>
2023-06-15 12:34:56 +0400

    Small changes 2

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -50,4 +50,47 @@ class AuthController
             return ['error' => $e->getMessage()];
         }
     }
+
+    /**
+     * @throws \Exception
+     */
+    public static function register () : string | array
+    {
+        $email = Request::post( 'email' );
+        $password = Request::post( 'password' );
+
+        if ( ! filter_var( $email , FILTER_VALIDATE_EMAIL ) )
+        {
+            Response::setStatusBadRequest();
+            return [ 'error' => 'Email is not correct.' ];
+        }
+
+        $password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*.-]).{8,}$/";
+
+        if ( empty( $password ) || ! preg_match( $password_regex, $password ) )
+        {
+            Response::setStatusBadRequest();
+            return [ 'error' => 'Password does not meet requirements. The length should be at least 8 character. Please, use a-z, A-Z, numbers and at least one special character.' ];
+        }
+
+        $hashedPassword = password_hash( $password, PASSWORD_DEFAULT );
+
+        try
+        {
+            $userId = User::createUser( $email, $hashedPassword );
+            $expiresAt = time() + 60 * 60 * 24 * 7;
+
+            $jwt = AuthHelper::generateJWT( [ "userId" => $userId, "exp" => $expiresAt ] );
+
+            setcookie( 'auth_token', $jwt, $expiresAt );
+        }
+        catch ( \Exception $e )
+        {
+            Response::setStatusConflict();
+            return ['error' => $e->getMessage() ];
+        }
+
+        Response::setStatusCreated();
+        return [ 'user' => [ 'id' => $userId, 'email' => $email ] ];
+    }
 }
