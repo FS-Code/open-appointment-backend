9969cabee861bb4d2a12ef5fb15fb46abce82e51
nasrinette <97260770+nasrinette@users.noreply.github.com>
2023-06-22 19:22:08 +0400

    issue 131

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 use App\Controllers\AuthController;
-
+use App\Controllers\ServiceController;
 use App\Core\Router;
 
 
@@ -15,6 +15,6 @@ Router::group( 'api', function() {
 	Router::post( 'register', [ AuthController::class, 'register' ] );
     Router::post( 'login', [ AuthController::class, 'login' ] );
     Router::post( 'get-timeslots', [ \App\Controllers\AppointmentController::class, 'getAllTimeslots' ] );
-
+    Router::post('create-service',[ServiceController::class, 'create']);
     Router::post('me', [AuthController::class, 'me'], [\App\Middlewares\AuthMiddleware::class]);
 } );
